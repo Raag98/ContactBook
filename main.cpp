b7e13794e41af702e8474072598aeafdcac87842
Anurag Rai <raianurag9800@gmail.com>
2020-12-08 19:18:34 +0000

    Commit 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void printStars() {
+  cout << "***************************" << endl;
+}
+
+void projCredits() {
+  cout << endl << "Project: CONTACT MANAGEMENT SYSTEM \n Made by:\n 1. Paras Aghija (191030) \n 2. Gautam Sachdeva(19103084) \n 3. Anurag Rai(19103065)" << endl;
+}
+
 string upperToLower(string S) {
   for(auto i = S.begin(); i != S.end(); i++) {
     if(*i >= 65 && *i <= 90)
@@ -50,11 +58,12 @@ public:
   }
 
 //  Updates Phone No.
-  void updatePNo(string S) {
+  void updatePNo(string old_pno, string new_pno) {
     // if(phone_numbers.empty()) 
     //   phone_numbers.push_back(S);
     // else 
-    phone_numbers[0] = S;
+    auto it = find(phone_numbers.begin(), phone_numbers.end(), old_pno);
+    *it = new_pno;
   }
 
   
@@ -148,7 +157,7 @@ public:
   }
 
   // Update Phone Number Function
-  Contact* update(string w1, string w2, string new_pno) {
+  Contact* update(string w1, string w2, string old_pno, string new_pno) {
     string w = w1 + w2;
     w = upperToLower(w);
     Node* temp = root;
@@ -159,7 +168,7 @@ public:
       }
       else temp = temp->children[ch];        
     }
-    if(temp->person) temp->person->updatePNo(new_pno);
+    if(temp->person) temp->person->updatePNo(old_pno, new_pno);
 
     return temp->person;
   }
@@ -278,7 +287,7 @@ public:
   }
 
   void update(string fname, string lname, string oldpno, string newpno) {
-    Contact *x = T.update(fname, lname, newpno);
+    Contact *x = T.update(fname, lname,oldpno, newpno);
     T2.update(oldpno, newpno, x);
   }
 
@@ -306,70 +315,9 @@ public:
 // Driver Code
 int main(){
 
-  // string fname1 = "Anurag";
-  // string lname1 = "Rai";
-  // string phone1 = "99887766";
-  // string email1 = "anumeraraag@gmail.com";
-  // string add1 = "123-/35 B";
   
-  // string fname2 = "Gautam";
-  // string lname2 = "Sir";
-  // string phone2 = "999888888";
-  // string email2 = "gautamsir@gmail.com";
-  // string add2 = "111-/22222 B";
-  
-  // string fname3 = "Paras";
-  // string lname3 = "Aghija";
-  // string phone3 = "111111111";
-  // string email3 = "paras@gmail.com";
-  // string add3 = "3333-/6666 B";
-
-  // string fname4 = "Shaan";
-  // string lname4 = "Chu";
-  // string phone4 = "666666999999";
-  // string email4 = "shaanthegandu@gmail.com";
-  // string add4 = "Kotha 69, GB Road";
-
-  // string fname5 = "Shaan";
-  // string lname5 = "lodu";
-  // string phone5 = "2222222222222";
-  // string email5 = "shaanlodulalit@gmail.com";
-  // string add5 = "Kotha 96, GB Road";
-  
-
-
-  
-
   // Trie t;
   // Trie2 t2;
-
-  // Contact *a1 = t.insert(fname1,lname1,phone1,email1);
-  // Contact *a2 = t.insert(fname2,lname2,phone2,email2);
-  // Contact *a3 = t.insert(fname3,lname3,phone3,email3);
-  // Contact *a4 = t.insert(fname4,lname4,phone4,email4);
-  // Contact *a5 = t.insert(fname5,lname5,phone5,email5);
-
-  // t2.insert(phone1,a1);
-  // t2.insert(phone2,a2);
-  // t2.insert(phone3,a3);
-  // t2.insert(phone4,a4);
-  // t2.insert(phone5,a5);
-
-  // t.find("Shaan");
-  
-  // t.find("Anurag");
-
-  // t.update("Anurag", "Rai", "9807075828");
-
-  // t.find("Anurag");
-
-  // t.addPhoneNumber("Anurag", "Rai", "1111111111");
-
-  // t.find("Anurag");
-
-  // string st = "AnUrAg";
-  // string st2 = upperToLower(st);
-  // cout << st2;
 
   // t2.find("111111111");
   // Contact *x = t.update("Paras", "Aghija", "0000000000");
@@ -390,20 +338,19 @@ int main(){
   // y.findByName("Anu");
   // y.findByPhoneNumber("0000000000");
 
-  // contactBook.printAllContacts();
-
   ContactBook contactBook;
   contactBook.insert("Paras", "Aghija", "9999988888", "aghija.paras@gmail.com", "Delhi", "19 August");
   contactBook.insert("Gautam", "Sachdeva", "7777788888", "sachdeva.gautam@gmail.com", "Allahabad", "23 June"); // Update DOB
   contactBook.insert("Anurag", "Rai", "7777766666", "rai.anurag@gmail.com", "Varanasi", "9 August");
   contactBook.insert("Shaan", "Grover", "5555566666", "grover.shaan@gmail.com", "Delhi", "9 August");
 
-
-  cout << "***************************" << endl << " CONTACT MANAGEMENT SYSTEM" << endl << "***************************" << endl;
+  printStars();
+  cout << " CONTACT MANAGEMENT SYSTEM" << endl;
+  printStars();
 
   while(1) {
     int opt;
-    cout << endl << "Features:\n 1 - Add Contact \n 2 - Delete Contact \n 3 - Search Contact \n 4 - Update Contact \n 5 - All Contacts \n 6 - Exit" << endl;
+    cout << endl << "Features:\n 1 - Add Contact \n 2 - Delete Contact \n 3 - Search Contact \n 4 - Update Contact \n 5 - All Contacts \n 6 - Project Details \n 7 - Exit" << endl;
     cout << "Enter your Choice: ";
     cin >> opt; 
 
@@ -427,8 +374,10 @@ int main(){
         break; 
       }
 
-      case 2: {
-        // Contact Deletion
+      case 2: { // Contact Deletion
+        string newPno;
+        cin >> newPno;
+        contactBook.addPhoneNumber("Shaan", "Grover", newPno);
         break;
       }
 
@@ -444,15 +393,23 @@ int main(){
       }
 
       case 4: {
-        string findCont_fname,findCont_lname, newPno;
+        string findCont_fname,findCont_lname, oldPno, newPno;
         cout << "Enter First Name of Contact: ";
         cin >> findCont_fname;
         cout << "Enter Last Name of Contact: ";
         cin >> findCont_lname;
-        findCont_fname += findCont_lname;
+
+        cout << "Enter Old Phone Number";
+        cin >> oldPno;
         cout << "Enter New Phone Number";
         cin >> newPno;
-        
+
+        contactBook.update(findCont_fname, findCont_lname, oldPno, newPno);
+
+        // findCont_fname += findCont_lname;        
+        cout << "Contact Updated Successfully!" << endl; 
+        contactBook.findByName(findCont_fname);
+
         break;
       }
 
@@ -461,8 +418,8 @@ int main(){
         break;
       }
 
-      case 6: { // Project Credits
-        // Group Members.
+      case 6: { // Working
+        projCredits();
         break;
       }
 
@@ -473,7 +430,7 @@ int main(){
       }
 
       default: {
-        cout << "Enter Valid Option";
+        cout << "Enter a Valid Option";
         break;
       }
     }
